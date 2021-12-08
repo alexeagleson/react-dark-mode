6f923bf1b31f110c1bfc2234e36649e936cde180
Alex Eagleson <alexeagleson@gmail.com>
2021-12-08 01:19:48 -0500

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import DarkMode from "./DarkMode";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <DarkMode />
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
